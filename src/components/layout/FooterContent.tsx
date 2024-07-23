ccb8e70faeb192e8c5f5389a146c998fb1bf420e
Mark <mr.catborg@gmail.com>
2024-07-23 17:27:36 -0400

    Adjusted the footer content to center the text using inline styles instead of a utility class.

--- src/components/layout/FooterContent.tsx
+++ src/components/layout/FooterContent.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function FooterContent() {
   return (
-    <Text ta="center" size="sm">
+    <Text style={{ textAlign: 'center' }} size="sm">
       A <Link href="https://markury.dev">Markury</Link> Project 2024
     </Text>
   );
