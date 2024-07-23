44d90c316bdae4702987d30f31712a62d66c2908
Mark <mr.catborg@gmail.com>
2024-07-23 17:26:28 -0400

    Added a new `FooterContent` component that displays a centered text with a link to the Markury website.

---
+++ src/components/layout/FooterContent.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@mantine/core";
+import Link from "next/link";
+
+export function FooterContent() {
+  return (
+    <Text ta="center" size="sm">
+      A Markury Project 2024 | <Link href="https://markury.dev">markury.dev</Link>
+    </Text>
+  );
+}
